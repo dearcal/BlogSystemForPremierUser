70bd90c59a33aa6494ebfbbb78e6d0fe13c6c566
dearcal <avrilulala@gmail.com>
2020-11-03 23:28:35 +0800

    add  route handlers demo function

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/example/b',function(req,res,next) {
+  console.log('the response will be sent by the next function');
+  next();
+},function (req,res) {
+  res.send('hello from b')
+})
+
 module.exports = router;
