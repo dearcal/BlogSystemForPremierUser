01486c2556729d051e362f5cbdf9d6ab87632cb9
dearcal <avrilulala@gmail.com>
2020-11-05 11:53:39 +0800

    test default url

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  console.log("console command");
+  res.send("hello world");
 });
 
 router.get('/example/b',function(req,res,next) {
