9312505975dae3e5441d483459fd4d55f4ca68bd
dearcal <avrilulala@gmail.com>
2020-11-09 17:59:08 +0800

    add mongodb connect function

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,10 +1,36 @@
 const CONFIG = require('../app.config')
 const mongoose = require('mongoose')
-
-mongoose.connect(CONFIG.MONGODB.uri);
-
-const db = mongoose.connection;
+const autoIncrement = require('mongoose-auto-increment')
 
 // remove DeprecationWarning
-mongoose.set('useFindAndModify', false)
+mongoose.set('useFindAndModify', false);
+
+exports.mongoose = mongoose
+
+exports.connect = () => {
+    //connect
+    mongoose.connect(CONFIG.MONGODB.uri,{
+        useCreateIndex: true,
+		useNewUrlParser: true,
+		promiseLibrary: global.Promise
+    });
+
+    //error handling
+    mongoose.connection.on('error',function(err) {
+        console.error("connect failed: " + err);
+    })
+
+    //connection success handlind
+    mongoose.connection.once('open',()=>{
+        console.log('connect success!');
+    })
+
+    //increment counter
+    autoIncrement.initialize(mongoose.connection);
+
+    //return connection instance
+    return mongoose;
+
+}
+
 
