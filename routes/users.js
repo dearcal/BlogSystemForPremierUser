4ca1603c2fbf4a3cadf52d6f160266216b2e7996
dearcal <avrilulala@gmail.com>
2020-11-11 15:01:15 +0800

    register feature with username and password

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const crypto = require('crypto');
+const User = require('../models/userSchema');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,11 +11,34 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/login',(req,res,next)=>{
-  
+  res.send("login test api")
 })
 
 router.post('/register',(req,res,next)=>{
-  
+
+  let { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(403);
+  }
+
+  User.findOne({username:username})
+    .then(data => {
+      if (data) {
+        return res.json("user existed");
+      }
+      let user = new User({
+        username:username,
+        password:crypto.createHash('md5').update(password).digest('hex')
+      });
+      user.save().then(data => {
+        return res.json("registe success");
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    
+
 })
 
 module.exports = router;
